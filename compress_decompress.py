d63d505eb2773fe30e02a3c76457e5ffe5a8e957
Congrong Ren <ren.452@osu.edu>
2024-03-15 22:19:58 -0400

    Add files via upload

--- compress_decompress.py
+++ compress_decompress.py
@@ -1,23 +1,16 @@
 import h5py
-import netCDF4 as nc
 import numpy as np
 import networkx as nx
-import trimesh
 import pickle
+import trimesh
 from dahuffman import HuffmanCodec
 import zstd
 import time
 import matplotlib.pyplot as plt
+import argparse
+from cmse.cmse import *
 
 m = 16
-xi_percentage_range = [1e-1, 5e-2, 2e-2, 1e-2, 5e-3, 2e-3, 1e-3, 5e-4, 2e-4, 1e-4, 5e-5, 2e-5, 1e-5, 5e-6, 2e-6, 1e-6]
-# xi_range = [2e-5, 1e-5, 5e-6, 2e-6, 1e-6]
-dataset = "VFEM"  # one of "synthetic", "XGC", "MPAS", "LES_s", "LES_l", "F6", "VFEM"
-attribute = "pressure"
-'''
-"LES_s" or "LES_l": one of "temperature", "pressure"
-"TSLAB": one of "rho", "phi", "u", and "v"
-'''
 
 
 def preprocessing_write(dataset, attribute):
@@ -30,17 +23,21 @@ def preprocessing_write(dataset, attribute):
         num_cells = cells.shape[0]
         dual_graph = dualGraph4MeshTriCells(cells, num_cells)
         dim = 2
+    elif dataset == "synthetic":
+        with open("../../TimeVaryingMesh/case_studies/synthetic/dataGen/syn_mesh.pickle", "rb") as f:
+            nodes_coor, cells = pickle.load(f)
+        with open("../../TimeVaryingMesh/case_studies/synthetic/dataGen/values.pickle", "rb") as f:
+            func = pickle.load(f)[:, 0]
+        num_cells = cells.shape[0]
+        dual_graph = dualGraph4MeshTriCells(cells, num_cells)
+        dim = 2
     elif dataset == "MPAS":
-        f = nc.Dataset("datasets/" + dataset + "/output.nc")
-        nodes_coor = np.concatenate((np.array(f.variables["xCell"]).reshape(-1, 1),
-                                     np.array(f.variables["yCell"]).reshape(-1, 1),
-                                     np.array(f.variables["zCell"]).reshape(-1, 1)), axis=1)
-        cells = np.array(f.variables['cellsOnVertex'])
-        reindex = np.where(cells == 0)
-        for i in range(reindex[0].shape[0]):
-            cells[reindex[0][i]][reindex[1][i]] = 1
-        cells -= 1
-        func = np.array(f.variables[attribute])[0, :, 0]
+        fname = "datasets/" + dataset + "/" + dataset + ".pickle"
+        with open(fname, "rb") as f:
+            data = pickle.load(f)
+        nodes_coor = data["nodes_coor"]
+        cells = data["cells"]
+        func = data[attribute]
         num_cells = cells.shape[0]
         dual_graph = dualGraph4MeshTriCells(cells, num_cells)
         dim = 2
@@ -54,8 +51,7 @@ def preprocessing_write(dataset, attribute):
         num_cells = cells.shape[0]
         dual_graph = dualGraph4MeshTetCells(cells, num_cells)
         dim = 3
-    num_nodes = nodes_coor.shape[0]
-    node2cells = node2cellsDict(cells, num_nodes)
+    node2cells = node2cellsDict(cells)
     func.tofile("datasets/" + dataset + "/" + attribute + "_binary.dat")
     with open("datasets/" + dataset + "/" + "dual.pickle", "wb") as f:
         pickle.dump(dual_graph, f)
@@ -72,17 +68,20 @@ def preprocessing_read(dataset, attribute):
         with h5py.File("../../TimeVaryingMesh/xgc/xgc.3d.00165.h5", "r") as f:
             func = np.array(f[attribute])[:, 0]
         dim = 2
+    elif dataset == "synthetic":
+        with open("../../TimeVaryingMesh/case_studies/synthetic/dataGen/syn_mesh.pickle", "rb") as f:
+            nodes_coor, cells = pickle.load(f)
+        with open("../../TimeVaryingMesh/case_studies/synthetic/dataGen/values.pickle", "rb") as f:
+            func = pickle.load(f)[:, 0]
+        dim = 2
     elif dataset == "MPAS":
-        f = nc.Dataset("datasets/" + dataset + "/output.nc")
-        nodes_coor = np.concatenate((np.array(f.variables["xCell"]).reshape(-1, 1),
-                                     np.array(f.variables["yCell"]).reshape(-1, 1),
-                                     np.array(f.variables["zCell"]).reshape(-1, 1)), axis=1)
-        cells = np.array(f.variables['cellsOnVertex'])
-        reindex = np.where(cells == 0)
-        for i in range(reindex[0].shape[0]):
-            cells[reindex[0][i]][reindex[1][i]] = 1
-        cells -= 1
-        func = np.array(f.variables[attribute])[0, :, 0]
+        fname = "datasets/" + dataset + "/" + dataset + ".pickle"
+        with open(fname, "rb") as f:
+            data = pickle.load(f)
+        nodes_coor = data["nodes_coor"]
+        cells = data["cells"]
+        # func = data[attribute]
+        func = np.fromfile("datasets/" + dataset + "/" + attribute + "_binary.dat")
         dim = 2
     else:
         fname = "datasets/" + dataset + "/" + dataset + ".pickle"
@@ -90,7 +89,8 @@ def preprocessing_read(dataset, attribute):
             data = pickle.load(f)
         nodes_coor = data["nodes_coor"]
         cells = data["cells"]
-        func = data[attribute]
+        # func = data[attribute]
+        func = np.fromfile("datasets/" + dataset + "/" + attribute + "_binary.dat")
         dim = 3
     with open("datasets/" + dataset + "/" + "dual.pickle", "rb") as f:
         dual_graph = pickle.load(f)
@@ -99,35 +99,27 @@ def preprocessing_read(dataset, attribute):
     return nodes_coor, cells, func, dual_graph, node2cells, dim
 
 
-def plotMetrics(dataset, attribute, xi_percentage_range, eval_metrics):
-    plot_range = 100 * np.array(xi_percentage_range)
-    fig, axs = plt.subplots(2, 4, figsize=(15, 8))
-    axs[0, 0].plot(plot_range, eval_metrics["runtime"][:, 0])
-    axs[0, 0].set_xlabel("error bound (%)")
-    axs[0, 0].set_ylabel("runtime (compression)")
-    axs[0, 1].plot(plot_range, eval_metrics["runtime"][:, 1])
-    axs[0, 1].set_xlabel("error bound (%)")
-    axs[0, 1].set_ylabel("runtime (decompression)")
-    axs[0, 2].plot(plot_range, eval_metrics["nrmse"])
-    axs[0, 2].set_xlabel("error bound (%)")
-    axs[0, 2].set_ylabel("NRMSE")
-    axs[0, 3].plot(plot_range, eval_metrics["psnr"])
-    axs[0, 3].set_xlabel("error bound (%)")
-    axs[0, 3].set_ylabel("PSNR (dB)")
-    axs[1, 0].plot(plot_range, eval_metrics["cr"])
-    axs[1, 0].set_xlabel("error bound (%)")
-    axs[1, 0].set_ylabel("compression ratio")
-    axs[1, 1].plot(plot_range, eval_metrics["br"])
-    axs[1, 1].set_xlabel("error bound (%)")
-    axs[1, 1].set_ylabel("bit rate")
-    axs[1, 2].plot(eval_metrics["br"], eval_metrics["nrmse"])
-    axs[1, 2].set_xlabel("bit rate")
-    axs[1, 2].set_ylabel("NRMSE")
-    axs[1, 3].plot(eval_metrics["br"], eval_metrics["psnr"])
-    axs[1, 3].set_xlabel("bit rate")
-    axs[1, 3].set_ylabel("PSNR (dB)")
+def plotMetrics(dataset, attribute, xi_pct_range, eval_metrics):
+    plot_range = 100 * np.array(xi_pct_range)
+    fig, axs = plt.subplots(1, 4, figsize=(15, 4))
+    axs[0].plot(plot_range, eval_metrics["runtime"][:, 0], c='dodgerblue', label="compression")
+    axs[0].plot(plot_range, eval_metrics["runtime"][:, 1], c='dodgerblue', linestyle="dashed", label="decompression")
+    axs[0].set_xlabel("error bound (%)")
+    axs[0].set_ylabel("runtime")
+    axs[0].legend()
+    axs[1].plot(plot_range, eval_metrics["nrmse"], c='dodgerblue', label="NRMSE")
+    axs[1].plot(plot_range, eval_metrics["cnrmse"], c='dodgerblue', linestyle="dashed", label="CNRMSE")
+    axs[1].set_xlabel("error bound (%)")
+    axs[1].set_ylabel("NRMSE / CNRMSE")
+    axs[2].plot(eval_metrics["br"], eval_metrics["psnr"], c='dodgerblue', label="PSNR")
+    axs[2].plot(eval_metrics["br"], eval_metrics["cpsnr"], c='dodgerblue', linestyle="dashed", label="CPSNR")
+    axs[2].set_xlabel("bit rate")
+    axs[2].set_ylabel("PSNR / CPSNR (dB)")
+    axs[3].plot(plot_range, eval_metrics["cr"])
+    axs[3].set_xlabel("error bound (%)")
+    axs[3].set_ylabel("compression ratio")
     fig.suptitle(dataset + " data - " + attribute)
-    plt.savefig("results/" + dataset + "/" + attribute + ".png")
+    plt.savefig("results/" + dataset + "/" + dataset + "_" + attribute + ".png")
 
 
 def dualGraph4MeshTetCells(cells, num_cells):
@@ -190,17 +182,18 @@ def dualGraph4MeshTriCells(cells, num_cells):
     return dual_graph
 
 
-def node2cellsDict(cells, num_nodes):
+def node2cellsDict(cells):
     node2cells = {}
-    for n in range(num_nodes):
-        node2cells[n] = []
     for i, c in enumerate(cells):
         for n in c:
-            node2cells[n].append(i)
+            try:
+                node2cells[n].append(i)
+            except:
+                node2cells[n] = [i]
     return node2cells
 
 
-def barycentric_interp(point_id, cell_id, nodes_coor, cells, values, dim):
+def barycentric_extrap(point_id, cell_id, nodes_coor, cells, values, dim):
     if dim == 2:
         nodes_of_cell = cells[cell_id]
         cell_nodes_coor = np.zeros((1, 3, 3))
@@ -215,8 +208,8 @@ def barycentric_interp(point_id, cell_id, nodes_coor, cells, values, dim):
             values[nodes_of_cell[2]] * barycentric_coor[0][2]
     T = np.zeros((3, 3))
     for i in range(3):
-        T[:, i] = nodes_coor[cells[cell_id][i]] - nodes_coor[cells[cell_id][3]]
-    a0, a1, a2 = np.dot(np.linalg.inv(T), nodes_coor[point_id] - nodes_coor[cells[cell_id][3]])
+        T[:, i] = nodes_coor[cells[cell_id][i]] - nodes_coor[cells[cell_id][-1]]
+    a0, a1, a2 = np.dot(np.linalg.inv(T), nodes_coor[point_id] - nodes_coor[cells[cell_id][-1]])
     a3 = 1 - a0 - a1 - a2
     return a0 * values[cells[cell_id][0]] + a1 * values[cells[cell_id][1]] + a2 * values[cells[cell_id][2]] + a3 * \
         values[cells[cell_id][3]]
@@ -319,7 +312,7 @@ def DFS_compression_oneSeed(data2compressed: list, xi: float, dim: int, graph: n
         stack_set.remove((current_cell, prev_cell))
         if visited_cells[current_cell] == 0:  # visited constraint
             node_to_update = list(set(cells[current_cell]) - set(cells[prev_cell]))[0]
-            pred_f = barycentric_interp(node_to_update, prev_cell, nodes_coor, cells, decomp_func, dim)  # predictor
+            pred_f = barycentric_extrap(node_to_update, prev_cell, nodes_coor, cells, decomp_func, dim)  # predictor
             code_distance = int((func[node_to_update] - pred_f) / xi)  # quantization
             if code_distance < 0:
                 code = 2 ** (m - 1) + int(np.floor(code_distance / 2))
@@ -337,7 +330,7 @@ def DFS_compression_oneSeed(data2compressed: list, xi: float, dim: int, graph: n
         neighbors = list(graph.neighbors(current_cell))
         neighbors.sort(reverse=True)  # search strategy: start with cell with min index
         for t in neighbors:
-            if visited_cells[t] == 0 and t not in stack_set:
+            if visited_cells[t] == 0 and (t, current_cell) not in stack_set:
                 stack.append((t, current_cell))
                 stack_set.add((t, current_cell))
     data2compressed.append(np.inf)
@@ -441,7 +434,7 @@ def DFS_decompression_oneSeed(decompressed_sequence: list, xi: float, dim: int, 
         stack_set.remove((current_cell, prev_cell))
         if visited_cells[current_cell] == 0:  # visited constraint
             node_to_update = list(set(cells[current_cell]) - set(cells[prev_cell]))[0]
-            pred_f = barycentric_interp(node_to_update, prev_cell, nodes_coor, cells, decomp_func, dim)
+            pred_f = barycentric_extrap(node_to_update, prev_cell, nodes_coor, cells, decomp_func, dim)
             code = decompressed_sequence[current_length]
             code_distance = code - 2 ** (m - 1)
             visited_cells, unvisited_cells, visited_nodes, num_visited_nodes = \
@@ -452,30 +445,52 @@ def DFS_decompression_oneSeed(decompressed_sequence: list, xi: float, dim: int, 
         neighbors = list(graph.neighbors(current_cell))
         neighbors.sort(reverse=True)  # search strategy: start with cell with min index
         for t in neighbors:
-            if visited_cells[t] == 0 and t not in stack_set:
+            if visited_cells[t] == 0 and (t, current_cell) not in stack_set:
                 stack.append((t, current_cell))
                 stack_set.add((t, current_cell))
     return visited_cells, unvisited_cells, visited_nodes, num_visited_nodes, decomp_func
 
 
 if __name__ == "__main__":
+    xi_pct_range = [1e-1, 5e-2, 2e-2, 1e-2, 5e-3, 2e-3, 1e-3, 5e-4, 2e-4, 1e-4, 5e-5, 2e-5, 1e-5, 5e-6, 2e-6, 1e-6]
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-dataset', default="LES_l", type=str)
+    parser.add_argument('-attribute', default="pressure", type=str)
+    args = parser.parse_args()
+    dataset = args.dataset
+    attribute = args.attribute
+    # dataset: one of "synthetic", "XGC", "MPAS", "LES_s", "LES_l", "F6", "VFEM", "car", "NYX"
+    ''' attribute
+        "XGC": "dneOverne0"
+        "LES_s", "LES_l", or "MPAS": one of "temperature", "pressure"
+        "F6", "VFEM", or "car": "pressure"
+        "TSLAB": one of "rho", "phi", "u", and "v"
+    '''
+
     t0 = time.perf_counter()
     nodes_coor, cells, func, dual, node2cells, dim = preprocessing_write(dataset, attribute)
     # nodes_coor, cells, func, dual, node2cells, dim = preprocessing_read(dataset, attribute)
     num_nodes = nodes_coor.shape[0]
     max_signal = np.max(func)
     min_signal = np.min(func)
-    xi_dim = len(xi_percentage_range)
+    xi_dim = len(xi_pct_range)
     runtime = np.zeros((xi_dim, 2))
+    mse = np.zeros(xi_dim)
+    rmse = np.zeros(xi_dim)
     nrmse = np.zeros(xi_dim)
     psnr = np.zeros(xi_dim)
+    cmse = np.zeros(xi_dim)
+    crmse = np.zeros(xi_dim)
+    cnrmse = np.zeros(xi_dim)
+    cpsnr = np.zeros(xi_dim)
     compression_ratio = np.zeros(xi_dim)
     bit_rate = np.zeros(xi_dim)
     t1 = time.perf_counter()
     print("Preprocessing Done! Time:", t1 - t0)
 
-    for i, xi_percentage in enumerate(xi_percentage_range):
-        xi = xi_percentage * (max_signal - min_signal)
+    for i, xi_pct in enumerate(xi_pct_range):
+        xi = xi_pct * (max_signal - min_signal)
         t0 = time.perf_counter()
         sequences, compressed_codes, cr, codec = DFS_compression(dual, xi, dim, nodes_coor, cells, node2cells, func)
         t1 = time.perf_counter()
@@ -485,22 +500,30 @@ if __name__ == "__main__":
         runtime[i, 1] = t2 - t1
         compression_ratio[i] = cr
         bit_rate[i] = 64 / cr
-        mse = np.sum((decompressed_vals - func) ** 2) / num_nodes
-        nrmse[i] = np.sqrt(mse) / (max_signal - min_signal)
-        psnr[i] = 20 * np.log10(max_signal) - 10 * np.log10(mse)
+        mse[i] = np.sum((decompressed_vals - func) ** 2) / num_nodes
+        rmse[i] = np.sqrt(mse[i])
+        nrmse[i] = rmse[i] / (max_signal - min_signal)
+        psnr[i] = 20 * np.log10(max_signal - min_signal) - 10 * np.log10(mse[i])
+        if dim == 2:
+            cmse[i] = triangularMeshCMSE(nodes_coor, cells, func, decompressed_vals)
+        else:
+            cmse[i] = tetraheronMeshCMSE(nodes_coor, cells, func, decompressed_vals)
+        crmse[i] = np.sqrt(cmse[i])
+        cnrmse[i] = crmse[i] / (max_signal - min_signal)
+        cpsnr[i] = - 20 * np.log10(cnrmse[i])
 
-        with open("results/" + dataset + "/" + attribute + "_compressed_" + str(xi_percentage) + ".dat", "wb") as f:
+        with open("results/" + dataset + "/" + attribute + "_compressed_" + str(xi_pct) + ".dat", "wb") as f:
             f.write(compressed_codes)
-        with open("results/" + dataset + "/" + attribute + "_sequences_" + str(xi_percentage) + ".pickle", "wb") as f:
+        with open("results/" + dataset + "/" + attribute + "_sequences_" + str(xi_pct) + ".pickle", "wb") as f:
             pickle.dump(sequences, f)
-        with open("results/" + dataset + "/" + attribute + "_decompressed_vals_" + str(xi_percentage) + ".pickle",
-                  "wb") as f:
+        with open("results/" + dataset + "/" + attribute + "_decompressed_vals_" + str(xi_pct) + ".pickle", "wb") as f:
             pickle.dump(decompressed_vals, f)
 
         print(t1 - t0, t2 - t1)
-        print(i, xi_percentage)
+        print(i, xi_pct)
 
-    eval_metrics = {"runtime": runtime, "nrmse": nrmse, "psnr": psnr, "cr": compression_ratio, "br": bit_rate}
-    plotMetrics(dataset, attribute, xi_percentage_range, eval_metrics)
+    eval_metrics = {"runtime": runtime, "mse": mse, "rmse": rmse, "nrmse": nrmse, "psnr": psnr, "cmse": cmse,
+                    "crmse": crmse, "cnrmse": cnrmse, "cpsnr": cpsnr, "cr": compression_ratio, "br": bit_rate}
+    plotMetrics(dataset, attribute, xi_pct_range, eval_metrics)
     with open("results/" + dataset + "/" + attribute + "_eval_metrics.pickle", "wb") as f:
         pickle.dump(eval_metrics, f)
